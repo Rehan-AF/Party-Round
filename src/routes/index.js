5d1bb3201ff7c13213be53960788960654a3820c
Rehan-AF <af.rehan03@gmail.com>
2022-08-26 20:46:55 +0500

    navbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/header";
 import HomePage from "../pages/HomePage";
 
 const Routers = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
       </Routes>
