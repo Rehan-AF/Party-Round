e6c8af18d53c0842283ce5677fb01fa65d633515
Rehan-AF <af.rehan03@gmail.com>
2022-08-27 22:34:45 +0500

    check list card design

--- src/components/checkListCard/index.jsx
+++ src/components/checkListCard/index.jsx
@@ -6,17 +6,20 @@ const CheckListCard = () => {
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <div className={classes.subDiv}>
-        <SvgIcons check={true} className={classes.icons} />
-        <Typography className={classes.typo}>Generating SAFE</Typography>
-      </div>
-      <div className={classes.subDiv}>
-        <SvgIcons check={true} className={classes.icons} />
-        <Typography className={classes.typo}>Setting up round</Typography>
-      </div>
-      <div className={classes.subDiv}>
-        <SvgIcons check={true} className={classes.icons} />
-        <Typography className={classes.typo}>Round started</Typography>
+      <div className={classes.divOne}></div>
+      <div className={classes.divtwo}>
+        <div className={classes.subDiv}>
+          <SvgIcons check={true} className={classes.icons} />
+          <Typography className={classes.typo}>Generating SAFE</Typography>
+        </div>
+        <div className={classes.subDiv}>
+          <SvgIcons check={true} className={classes.icons} />
+          <Typography className={classes.typo}>Setting up round</Typography>
+        </div>
+        <div className={classes.subDiv}>
+          <SvgIcons check={true} className={classes.icons} />
+          <Typography className={classes.typo}>Round started</Typography>
+        </div>
       </div>
     </div>
   );
@@ -26,24 +29,38 @@ export default CheckListCard;
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
-    flexDirection: "column",
-    gap: "40px",
     width: "400px",
     backgroundColor: "#222",
-    padding: "32px",
+    padding: "0 32px 15px 32px",
     border: "2px solid hsla(0,0%,100%,.06)",
     borderRadius: "1.3rem",
     boxShadow: "0 4px 12px 6px rgb(0 0 0 / 24%)",
+    position: "relative",
+    height: "227px",
   },
   subDiv: {
     display: "flex",
     gap: "20px",
     fontSize: "1.1rem",
+    backgroundColor: "#222",
+    padding: "2px",
   },
   icons: {
     width: "30px",
   },
   typo: {
     fontSize: "1.7rem",
+  },
+  divOne: {
+    padding: "15px",
+    borderRight: "2px solid hsla(0,0%,100%,.06)",
+  },
+  divtwo: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "30px",
+    padding: "17px",
+    position: "absolute",
+    left: "30px",
   },
 }));
