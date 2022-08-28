a9effa7886c0c4556e770c05b418ffdb132d534e
Rehan-AF <af.rehan03@gmail.com>
2022-08-28 12:03:32 +0500

    sectionFour

---
+++ src/components/iosSwitch/index.jsx
@@ -0,0 +1,54 @@
+import { Switch, withStyles } from "@material-ui/core";
+
+export const IOSSwitch = withStyles((theme) => ({
+  root: {
+    width: 62,
+    height: 37,
+    padding: 0,
+    margin: theme.spacing(1),
+  },
+  switchBase: {
+    padding: 1,
+    "&$checked": {
+      transform: "translateX(26px)",
+      color: theme.palette.common.white,
+      "& + $track": {
+        backgroundColor: "#36e3b5",
+        opacity: 1,
+        border: "none",
+      },
+    },
+    "&$focusVisible $thumb": {
+      color: "#52d869",
+      border: "6px solid #fff",
+    },
+  },
+  thumb: {
+    width: 34,
+    height: 34,
+  },
+  track: {
+    borderRadius: 50 / 2,
+    border: `1px solid ${theme.palette.grey[400]}`,
+    backgroundColor: "#e9e9eb",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color", "border"]),
+  },
+  checked: {},
+  focusVisible: {},
+}))(({ classes, ...props }) => {
+  return (
+    <Switch
+      focusVisibleClassName={classes.focusVisible}
+      disableRipple
+      classes={{
+        root: classes.root,
+        switchBase: classes.switchBase,
+        thumb: classes.thumb,
+        track: classes.track,
+        checked: classes.checked,
+      }}
+      {...props}
+    />
+  );
+});
---
+++ src/components/section4/index.jsx
@@ -0,0 +1,48 @@
+import { makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import ToggleCard from "../togglecard";
+const SectionFour = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div>
+        <Typography className={classes.title}>
+          Raise in cash
+          <br /> or crypto
+          <br /> or both
+        </Typography>
+
+        <div>
+          <Typography className={classes.typo}>
+            with USD and USDC supporte, you can raise
+            <br /> from anyone,anywhere
+          </Typography>
+        </div>
+      </div>
+      <div className={classes.img}>
+        <ToggleCard />
+      </div>
+    </div>
+  );
+};
+
+export default SectionFour;
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    position: "relative",
+    justifyContent: "space-between",
+  },
+  title: {
+    lineHeight: "1",
+    fontSize: "4rem",
+    fontWeight: "bold",
+  },
+  typo: {
+    marginTop: "32px",
+    fontSize: "1.2rem",
+    lineHeight: "1.1",
+    color: "hsla(0,0%,100%,.8)",
+  },
+}));
